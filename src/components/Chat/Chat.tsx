c9094015a716d02ff33ac6e081546244ce1dea09
igor <igorek888rane@gmail.com>
2023-02-06 18:37:28 +0300

    chat

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -1,8 +1,14 @@
 import { FC } from 'react'
 import styles from './Chat.module.scss'
+import { useParams } from 'react-router-dom'
 
 const Chat: FC = () => {
-	return <div className={styles.chat_block}>chat</div>
+	const params = useParams()
+	return (
+		<div className={styles.chat_block}>
+			<div className={styles.header}>{params.username}</div>
+		</div>
+	)
 }
 
 export default Chat
