f872183e218edb8d85203e12bcadce4605f46301
igor <igorek888rane@gmail.com>
2023-02-09 12:55:15 +0300

    no messages

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -5,12 +5,15 @@ import Message from './Message/Message'
 import MessageInputField from './MessageInputField/MessageInputField'
 import { useAppDispatch, useAppSelector } from '../../hooks/useApp'
 import { screenWidthCheck } from '../../utils/screenWidthCheck'
+import { CgHello } from 'react-icons/all'
+import { setMessages } from '../../store/messagesSlice/messagesSlice'
 
 const Chat: FC = () => {
 	const params = useParams()
 	const intoViewRef = useRef<HTMLDivElement>(null)
 	const { messages } = useAppSelector(state => state.messages)
 	const { zIndex } = useAppSelector(state => state.zIndex)
+	const { id } = useAppSelector(state => state.auth)
 	const dispatch = useAppDispatch()
 	useEffect(() => {
 		intoViewRef.current?.scrollIntoView({
@@ -31,14 +34,42 @@ const Chat: FC = () => {
 				</NavLink>
 				<h1>{params.username}</h1>
 			</div>
-			<div className={styles.chat_area}>
-				{messages.map(el => (
-					<Message
-						userId={el.userId}
-						message={el.message}
-						key={el.id}
-					/>
-				))}
+			<div
+				className={
+					messages.length ? styles.chat_area : styles.messages_not
+				}
+			>
+				{messages.length ? (
+					messages.map(el => (
+						<Message
+							userId={el.userId}
+							message={el.message}
+							key={el.id}
+						/>
+					))
+				) : (
+					<div className={styles.messages_not}>
+						<h1>No messages</h1>
+						<p>Send a message or click on the greeting below</p>
+						<CgHello
+							onClick={() =>
+								dispatch(
+									setMessages({
+										id: Date.now(),
+										userId: id,
+										message: 'Hello',
+									})
+								)
+							}
+							size={35}
+							style={{
+								color: '#ffff',
+								marginTop: '10',
+								cursor: 'pointer',
+							}}
+						/>
+					</div>
+				)}
 				<div ref={intoViewRef}></div>
 			</div>
 			<MessageInputField />
